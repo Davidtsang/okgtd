fff49abb54e838d33131648cf30dced96066827b
david <david.zen.2007@gmail.com>
2013-12-31 13:52:10 +0800

    production test

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20131224075056) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "stuffs", force: true do |t|
     t.string   "content"
     t.integer  "statu_code"
@@ -25,14 +28,14 @@ ActiveRecord::Schema.define(version: 20131224075056) do
     t.integer  "user_id"
   end
 
-  add_index "stuffs", ["user_id"], name: "index_stuffs_on_user_id"
+  add_index "stuffs", ["user_id"], name: "index_stuffs_on_user_id", using: :btree
 
   create_table "stuffs_tags", force: true do |t|
     t.integer "stuff_id", null: false
     t.integer "tag_id",   null: false
   end
 
-  add_index "stuffs_tags", ["stuff_id", "tag_id"], name: "index_stuffs_tags_on_stuff_id_and_tag_id", unique: true
+  add_index "stuffs_tags", ["stuff_id", "tag_id"], name: "index_stuffs_tags_on_stuff_id_and_tag_id", unique: true, using: :btree
 
   create_table "tags", force: true do |t|
     t.string   "name"
@@ -49,8 +52,8 @@ ActiveRecord::Schema.define(version: 20131224075056) do
     t.string   "remember_token"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email"
-  add_index "users", ["password_digest"], name: "index_users_on_password_digest"
-  add_index "users", ["remember_token"], name: "index_users_on_remember_token"
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
+  add_index "users", ["password_digest"], name: "index_users_on_password_digest", using: :btree
+  add_index "users", ["remember_token"], name: "index_users_on_remember_token", using: :btree
 
 end
