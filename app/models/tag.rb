c474c3748cdf4fdbe1c75ed041be3a6ed10462f8
david <david.zen.2007@gmail.com>
2014-01-02 14:30:35 +0800

    fix pqgsl group error

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -6,12 +6,12 @@ class Tag < ActiveRecord::Base
 
   def Tag.folder_tags(user,folder_code)
     Tag.find_by_sql(["
-        SELECT st.[tag_id],t.name,COUNT(st.tag_id) AS stuffs_num
+        SELECT st.tag_id,t.name,COUNT(st.tag_id) AS stuffs_num
     FROM stuffs_tags as st
     JOIN tags AS t ON t.id = st.tag_id
-    JOIN stuffs AS s ON s.[id] = st.stuff_id
-    WHERE s.[statu_code] = ? AND t.[user_id] =?
-    GROUP BY st.tag_id", folder_code,user.id
+    JOIN stuffs AS s ON s.id = st.stuff_id
+    WHERE s.statu_code = ? AND t.user_id =?
+    GROUP BY st.tag_id ,t.name", folder_code,user.id
     ])
   end
 
